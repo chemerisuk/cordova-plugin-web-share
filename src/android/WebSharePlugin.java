7269f4431cd99063f2ccd755621eb4b260193d73
Maksim Chemerisiuk <chemerisuk@gmail.com>
2023-04-15 14:17:58 +0200

    Improve android implementation to be more secure

--- src/android/WebSharePlugin.java
+++ src/android/WebSharePlugin.java
@@ -1,13 +1,9 @@
 package by.chemerisuk.cordova;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.ComponentName;
-import android.content.IntentFilter;
 import android.os.Build;
 
 import by.chemerisuk.cordova.support.CordovaMethod;
@@ -24,35 +20,20 @@ public class WebSharePlugin extends ReflectiveCordovaPlugin {
     private static final int SHARE_REQUEST_CODE = 18457896;
 
     private CallbackContext shareCallbackContext;
-    private BroadcastReceiver chosenComponentReceiver;
     private PendingIntent chosenComponentPI;
-    private ComponentName lastChosenComponent;
 
     @Override
     protected void pluginInitialize() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-            chosenComponentReceiver = new BroadcastReceiver() {
-                @Override
-                public void onReceive(Context context, Intent intent) {
-                    lastChosenComponent = (ComponentName)intent.getExtras().get(Intent.EXTRA_CHOSEN_COMPONENT);
-                }
-            };
-
-            cordova.getActivity().registerReceiver(chosenComponentReceiver, new IntentFilter(Intent.EXTRA_CHOSEN_COMPONENT));
-
-            chosenComponentPI = PendingIntent.getBroadcast(cordova.getActivity(),
-                SHARE_REQUEST_CODE + 1, new Intent(Intent.EXTRA_CHOSEN_COMPONENT), PendingIntent.FLAG_CANCEL_CURRENT | PendingIntent.FLAG_MUTABLE);
+        Context context = cordova.getActivity().getApplicationContext();
+        int flags = PendingIntent.FLAG_CANCEL_CURRENT;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S_V2) {
+            flags |= PendingIntent.FLAG_MUTABLE;
         }
+        chosenComponentPI = PendingIntent.getBroadcast(context,
+                SHARE_REQUEST_CODE + 1,
+                new Intent(context, WebShareReceiver.class), flags);
     }
 
-    @Override
-    public void onDestroy() {
-        if (chosenComponentReceiver != null) {
-            cordova.getActivity().unregisterReceiver(chosenComponentReceiver);
-        }
-    }
-
-    @SuppressLint("NewApi")
     @CordovaMethod
     protected void share(CordovaArgs args, CallbackContext callbackContext) throws JSONException {
         JSONObject options = args.getJSONObject(0);
@@ -69,29 +50,26 @@ public class WebSharePlugin extends ReflectiveCordovaPlugin {
         if (!title.isEmpty()) {
             sendIntent.putExtra(Intent.EXTRA_SUBJECT, title);
         }
-
         if (chosenComponentPI != null) {
+            WebShareReceiver.resetChosenComponent();
             sendIntent = Intent.createChooser(sendIntent, title, chosenComponentPI.getIntentSender());
-            lastChosenComponent = null;
         } else {
             sendIntent = Intent.createChooser(sendIntent, title);
         }
 
         cordova.startActivityForResult(this, sendIntent, SHARE_REQUEST_CODE);
-
-        this.shareCallbackContext = callbackContext;
+        shareCallbackContext = callbackContext;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == SHARE_REQUEST_CODE && this.shareCallbackContext != null) {
+        if (requestCode == SHARE_REQUEST_CODE && shareCallbackContext != null) {
             JSONArray packageNames = new JSONArray();
             if (resultCode == Activity.RESULT_OK) {
-                packageNames.put(lastChosenComponent != null ? lastChosenComponent.getPackageName() : "");
+                packageNames.put(WebShareReceiver.getChosenComponentPackage());
             }
-
-            this.shareCallbackContext.success(packageNames);
-            this.shareCallbackContext = null;
+            shareCallbackContext.success(packageNames);
+            shareCallbackContext = null;
         }
     }
 }
---
+++ src/android/WebShareReceiver.java
@@ -0,0 +1,27 @@
+package by.chemerisuk.cordova;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class WebShareReceiver extends BroadcastReceiver {
+    private static ComponentName lastChosenComponent;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        lastChosenComponent = (ComponentName)intent.getExtras().get(Intent.EXTRA_CHOSEN_COMPONENT);
+    }
+
+    public static void resetChosenComponent() {
+        lastChosenComponent = null;
+    }
+
+    public static String getChosenComponentPackage() {
+        if (lastChosenComponent != null) {
+            return lastChosenComponent.getPackageName();
+        } else {
+            return "";
+        }
+    }
+}
